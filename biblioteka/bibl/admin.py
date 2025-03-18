0d0b8988de96e4b9ceb24f884e9ff70a2cae5534
Ljubica <lubicaj724@gmail.com>
2025-03-18 14:50:40 +0100

    Completed

--- biblioteka/bibl/admin.py
+++ biblioteka/bibl/admin.py
@@ -1,50 +1,126 @@
 from django.contrib import admin
-from .models import Genre, Translator, Book, Rating, BookGenre, BookTranslator
+from .models import Translator, Genre, Book, Author, BookGenres, BookAuthors, BookTranslators, Rating
 
-class BookGenreInline(admin.TabularInline):
-    model = BookGenre
-    extra = 1
-
-class BookTranslatorInline(admin.TabularInline):
-    model = BookTranslator
-    extra = 1
 
 class RatingInline(admin.TabularInline):
+    exclude = ("user",)
     model = Rating
+    readonly_fields = ("user",)
+    extra = 2
+    def has_change_permission(self, request, obj=None):
+        return False
+    def has_delete_permission(self, request, obj=None):
+        # if obj is None or request.user != obj.added_by:
+        #     return True
+        # return any(rating.user == request.user for rating in obj.rating_set.all())
+        return True
+    def has_add_permission(self, request, obj=None):
+        if obj is None or request.user != obj.added_by:
+            return True
+        return request.user != obj.added_by
+        # i want to set the user of the rating object to be the user from the request, but rating is inline
+
+
+class BookGenresInline(admin.TabularInline):
+    model = BookGenres
     extra = 1
-    readonly_fields = ('user', 'rating', 'comment', 'created_at')
-    can_delete = True
 
-@admin.register(Book)
+    def has_change_permission(self, request, obj=None):
+        if obj is None or request.user == obj.added_by:
+            return True
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        if obj is None or request.user == obj.added_by:
+            return True
+        return False
+
+    def has_add_permission(self, request, obj=None):
+        if obj is None or request.user == obj.added_by:
+            return True
+        return False
+
+class BookAuthorsInline(admin.TabularInline):
+    model = BookAuthors
+    extra = 1
+
+    def has_change_permission(self, request, obj=None):
+        if obj is None or request.user == obj.added_by:
+            return True
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        if obj is None or request.user == obj.added_by:
+            return True
+        return False
+
+    def has_add_permission(self, request, obj=None):
+        if obj is None or request.user == obj.added_by:
+            return True
+        return False
+
+class BookTranslationsInline(admin.TabularInline):
+    model = BookTranslators
+    extra = 1
+    def has_change_permission(self, request, obj=None):
+        if obj is None or request.user == obj.added_by:
+            return True
+        return False
+    def has_delete_permission(self, request, obj=None):
+        if obj is None or request.user == obj.added_by:
+            return True
+        return False
+
+    def has_add_permission(self, request, obj=None):
+        if obj is None or request.user == obj.added_by:
+            return True
+        return False
+
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publish_date', 'added_by', 'page_count', 'is_available', 'average_rating_display')
-    list_filter = ('is_available',)
-    search_fields = ('title', 'author')
-    inlines = [BookGenreInline, BookTranslatorInline, RatingInline]
+    exclude = ("added_by",)
+    list_display = ("title", "issue_date", "added_by", "available",)
+    list_filter = ("available",)
+    inlines = [BookGenresInline, BookAuthorsInline, BookTranslationsInline, RatingInline]
 
-    def average_rating_display(self, obj):
-        avg = obj.average_rating()
-        return round(avg, 2) if avg else "No ratings"
-    average_rating_display.short_description = 'Avg. Rating'
+    def get_readonly_fields(self, request, obj=None):
+        if obj is None or obj.added_by == request.user:
+            return []
+        return ["title", "issue_date", "added_by", "available", "book_cover", "num_pages",]
 
-@admin.register(Genre)
-class GenreAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description')
+    # def has_change_permission(self, request, obj=None):
+    #     if obj is None:
+    #         return True
+    #     return obj.added_by == request.user
 
-@admin.register(Translator)
-class TranslatorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'nationality', 'birth_date')
+    def has_delete_permission(self, request, obj=None):
+        if obj is None:
+            return True
+        return obj.added_by == request.user
 
-@admin.register(Rating)
-class RatingAdmin(admin.ModelAdmin):
-    list_display = ('book', 'user', 'rating', 'created_at')
-    search_fields = ('book__title', 'user__username')
-    list_filter = ('rating',)
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.added_by = request.user
+        obj.save()
+        return super(BookAdmin, self).save_model(request, obj, form, change)
 
-@admin.register(BookGenre)
-class BookGenreAdmin(admin.ModelAdmin):
-    list_display = ('book', 'genre')
+    def save_formset(self, request, form, formset, change):
+        """Ensure Rating objects get the current user as the 'user' field, and allow deletions."""
+        instances = formset.save(commit=False)  # Get unsaved inline instances
 
-@admin.register(BookTranslator)
-class BookTranslatorAdmin(admin.ModelAdmin):
-    list_display = ('book', 'translator')
+        # Handle newly added instances
+        for instance in instances:
+            if isinstance(instance, Rating) and not instance.pk:
+                instance.user = request.user  # Assign current user
+            instance.save()  # Save each instance
+
+        # Handle deleted instances
+        for obj in formset.deleted_objects:
+            obj.delete()  # Explicitly delete removed objects
+
+        formset.save_m2m()  # Save many-to-many relationships if any
+
+
+admin.site.register(Translator)
+admin.site.register(Genre)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
--- biblioteka/bibl/models.py
+++ biblioteka/bibl/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Genre(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.name
-
 class Translator(models.Model):
     name = models.CharField(max_length=100)
     nationality = models.CharField(max_length=100)
@@ -16,44 +9,49 @@ class Translator(models.Model):
     def __str__(self):
         return self.name
 
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    nationality = models.CharField(max_length=100)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publish_date = models.DateField()
+    title = models.CharField(max_length=100)
+    # authors = models.ManyToManyField(Author)
+    # genres = models.ManyToManyField(Genre)
     added_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    page_count = models.PositiveIntegerField()
-    cover_image = models.ImageField(upload_to='covers/', null=True, blank=True)
-    is_available = models.BooleanField(default=True)
+    issue_date = models.DateField()
+    num_pages = models.IntegerField()
+    book_cover = models.ImageField(upload_to='book_cover/', null=True, blank=True)
+    available = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
 
-    def average_rating(self):
-        ratings = self.ratings.all()
-        if ratings.exists():
-            return sum(r.rating for r in ratings) / ratings.count()
-        return None
+class BookAuthors(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
 
-class BookGenre(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='book_genres')
+class BookGenres(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return f"{self.book.title} - {self.genre.name}"
-
-class BookTranslator(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='book_translators')
+class BookTranslators(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
     translator = models.ForeignKey(Translator, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return f"{self.book.title} - {self.translator.name}"
-
 class Rating(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='ratings')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    rating = models.IntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    rating = models.PositiveSmallIntegerField()
-    comment = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
     def __str__(self):
-        return f"{self.book.title} - {self.rating} by {self.user.username}"
+        return self.user.first_name + ", " + str(self.rating)
